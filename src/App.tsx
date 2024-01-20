2fe0baf5e60a5a7e691ce8c7b6e6d6a3c47303d0
Klimek Jakub <jakub.klimek@krakweb.pl>
2024-01-20 14:02:28 +0100

    screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesPage from "./pages/ArticlesPage";
+import LandingPage from "./pages/LandingPage";
+import DashboardPage from "./pages/DashboardPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage/>
+  },
+  {
+    path: "/artykuly",
+    element: <ArticlesPage/>,
+  },
+  {
+    path: "/artykuly/:id",
+    element: <ArticlePage/>,
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardPage/>,
+  },
+]);
+
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      Ekipa Remontowo Budowlana Borek Fałęcki
-    </div>
+      <RouterProvider router={router} />
   );
 };
 
